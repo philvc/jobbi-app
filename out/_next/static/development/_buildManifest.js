61ec12bfcd63d3281a2ea7a2f937f8312e6c5e53
philNight <philippe.van.caloen@nightborn.be>
2021-10-24 16:37:43 +0200

    feat: add next build & test capactior run device

---
+++ out/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/sign-in":["static\u002Fchunks\u002Fpages\u002Fauth\u002Fsign-in.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fauth\u002Fsign-in","\u002Fhome"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Page from "../components/shared/layout/page";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 import { Container, Flex, Stack } from "@chakra-ui/layout";
 import { useTranslation } from "react-i18next";
 import Paragraph from "../components/shared/typography/paragraph";
@@ -10,21 +10,14 @@ import { Form, Formik } from "formik";
 import Button from "../components/shared/actions/button";
 
 export default function Home() {
-	// Attributes
-	const router = useRouter();
-	const { t } = useTranslation();
+  // Attributes
+  const router = useRouter();
+  const { t } = useTranslation();
 
-	return (
-		<Formik<any> initialValues={{ tahir: "0494903665" }} onSubmit={(data) => alert(data.tahir)}>
-			<Form>
-				<Page p={4}>
-					<Stack spacing={4}>
-						<InputField placeholder="Numéro de téléphone" name="tahir" />
-						<InputField type="password" placeholder="Mot de passe" name="tahir" />
-						<Button type="submit" text="Se connecter" />
-					</Stack>
-				</Page>
-			</Form>
-		</Formik>
-	);
+  // Effects
+  useEffect(() => {
+    router.replace("/auth/sign-in");
+  });
+
+  return <Fragment />;
 }
