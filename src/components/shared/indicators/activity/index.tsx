8bfadae7eb79e468e33daee0f3772662e870e74b
philNight <philippe.van.caloen@nightborn.be>
2021-11-02 08:55:52 +0100

    feat: next version 12

--- src/components/shared/indicators/activity/index.tsx
+++ src/components/shared/indicators/activity/index.tsx
@@ -1,22 +1,26 @@
 import ActivityProps from "./props";
 import activityIndicatorAnimation from "./assets/activity-indicator.json";
-import Lottie from "lottie-react";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { Box, Container, Flex } from "@chakra-ui/layout";
 
-const Activity = ({ children, isLoading, height = "100vh", width = "100%" }: ActivityProps) => {
-	if (isLoading) {
-		return (
-			<Box width={width} height={height}>
-				<Flex align="center" justify="center">
-					<Box height={50} width={50}>
-						<Lottie animationData={activityIndicatorAnimation} height={height} width={width} />
-					</Box>
-				</Flex>
-			</Box>
-		);
-	}
-	return <>{children}</>;
+const Activity = ({
+  children,
+  isLoading,
+  height = "100vh",
+  width = "100%",
+}: ActivityProps) => {
+  if (isLoading) {
+    return (
+      <Box width={width} height={height}>
+        <Flex align="center" justify="center">
+          <Box height={50} width={50}>
+            <div>Loading...</div>{" "}
+          </Box>
+        </Flex>
+      </Box>
+    );
+  }
+  return <>{children}</>;
 };
 
 export default Activity;
